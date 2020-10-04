a55d317559ee8027e8250857b25731d9f358583f
David Čeliš <u19.404@pslib.cloud>
2020-10-04 21:23:27 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            int num2 = int.Parse(Console.ReadLine());
+            int nasob = num1 * num2;
+
+            
+            
+            }
+        }
+
+    }
+    }
+
